a212fb362f0f1ad7de6208ac456f7ed25ad19acb
Andrij Skoropad <andross_s@ukr.net>
2026-05-12 10:52:50 +0300

    fix style, add favicon, Mousewheel for Swiper

--- src/js/reviewsSwiper.js
+++ src/js/reviewsSwiper.js
@@ -1,13 +1,32 @@
 import Swiper from 'swiper';
+import { Mousewheel } from 'swiper/modules';
 import 'swiper/css';
 
 const reviewsSwiperElement = document.querySelector('.reviews-swiper');
 
 if (reviewsSwiperElement) {
-  new Swiper(reviewsSwiperElement, {
+  const desktopMediaQuery = window.matchMedia('(min-width: 1200px)');
+
+  const reviewsSwiper = new Swiper(reviewsSwiperElement, {
+    modules: [Mousewheel],
     loop: true,
     grabCursor: true,
     spaceBetween: 24,
     slidesPerView: 'auto',
+    mousewheel: {
+      enabled: desktopMediaQuery.matches,
+      releaseOnEdges: true,
+    },
   });
+
+  const toggleDesktopMousewheel = event => {
+    if (event.matches) {
+      reviewsSwiper.mousewheel.enable();
+      return;
+    }
+
+    reviewsSwiper.mousewheel.disable();
+  };
+
+  desktopMediaQuery.addEventListener('change', toggleDesktopMousewheel);
 }
